b3f418b91c001217773699fc1f05ddfc8387a081
AaravKabir <75346593+AaravKabir@users.noreply.github.com>
2022-11-06 18:12:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1 = "";
 song2 = "";
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -10,8 +15,23 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet();
+    posenet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Ml5 is initialised")
 }
 
 function draw() {
     image(video,0,0,600,500);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+    }
 }
